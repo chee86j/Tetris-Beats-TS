314c15aa585584ddeea6a0be7d14e0b1d7903524
chee86j <jeffreychee86@gmail.com>
2023-08-14 12:26:24 -0400

    react draggable controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 import {
   ArrowBigLeft,
   ArrowBigDownDash,
   ArrowBigRight,
   ArrowBigUp,
   RefreshCw,
+  Lock,
+  Unlock,
 } from "lucide-react";
 
 interface ControlsProps {
@@ -22,24 +25,42 @@ const Controls: React.FC<ControlsProps> = ({
   drop,
   hold,
 }) => {
+  const [isDraggable, setIsDraggable] = useState(false);
+
   return (
-    <div className="controls-container">
-      <button onClick={moveLeft}>
-        <ArrowBigLeft size={24} strokeWidth={2} />
-      </button>
-      <button onClick={moveRight}>
-        <ArrowBigRight size={24} strokeWidth={2} />
-      </button>
-      <button onClick={rotate}>
-        <ArrowBigUp size={24} strokeWidth={2} />
-      </button>
-      <button className="holdButton" onClick={hold}>
-        <RefreshCw size={24} strokeWidth={2} />
-      </button>
-      <button onClick={drop}>
-        <ArrowBigDownDash size={24} strokeWidth={2} />
-      </button>
-    </div>
+    <Draggable disabled={!isDraggable}>
+      <div className="controls-container">
+        {/* Directional Controls */}
+        <div className="directional-controls">
+          <button onClick={moveLeft}>
+            <ArrowBigLeft size={18} strokeWidth={2} />
+          </button>
+          <button onClick={rotate}>
+            <ArrowBigUp size={18} strokeWidth={2} />
+          </button>
+
+          <button onClick={moveRight}>
+            <ArrowBigRight size={18} strokeWidth={2} />
+          </button>
+        </div>
+
+        {/* A and B Buttons */}
+        <div className="ab-controls">
+          <button className="holdButton" onClick={hold}>
+            <RefreshCw size={18} strokeWidth={2} /> {/* A Button */}
+          </button>
+          <button onClick={drop}>
+            <ArrowBigDownDash size={18} strokeWidth={2} />
+          </button>
+          {/* Lock and Unlock Controls */}
+          <div className="lock-controls">
+            <button onClick={() => setIsDraggable(!isDraggable)}>
+              {isDraggable ? <Unlock size={18} /> : <Lock size={16} />}
+            </button>
+          </div>
+        </div>
+      </div>
+    </Draggable>
   );
 };
 
